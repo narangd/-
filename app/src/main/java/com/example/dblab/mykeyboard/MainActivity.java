2f8991a3a8c013064126cdd10081cdd1d7b2ea66
narangd <dnjstnddl1679@hanmail.net>
2016-05-23 12:04:51 +0900

    git remote add origin https://github.com/narangd/Hangul-SimpleQwerty

---
+++ app/src/main/java/com/example/dblab/mykeyboard/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.dblab.mykeyboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        if (editText != null)
+            editText.requestFocus();
+    }
+}
